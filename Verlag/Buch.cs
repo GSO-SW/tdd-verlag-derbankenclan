ee158c18a25062b753938c0810f108efcf28f7f8
Vincent <vincent.k8@gso.schule.konl>
2023-11-13 12:56:42 +0100

    Alle UnitTests bis auf den Letzten geschafft!

--- Verlag/Buch.cs
+++ Verlag/Buch.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Verlag
@@ -10,14 +11,68 @@ namespace Verlag
     {
         private string autor;
         private string titel;
-        private int auflage;
+        private int auflage = 1;
         public Buch (string autor, string titel, int auflage)
         {
-            this.autor = autor;
-            this.titel = titel;
-            this.auflage = auflage;
+            if (this.auflage <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Auflage darf nicht kleiner als 1 sein!");
+            }
+            else if (!Regex.IsMatch(autor, @"^[a-zA-Z]+$"))
+            {
+                throw new ArgumentOutOfRangeException("Name des Autors darf nur Buchstaben enthalten!");
+            }
+            else
+            {
+                this.autor = autor;
+                this.titel = titel;
+                this.auflage = auflage;
+            }
         }
 
+        public Buch (string autor, string titel)
+        {
+            if (!Result)
+            {
+                throw new ArgumentOutOfRangeException("Name des Autors darf nur Buchstaben enthalten!");
+            }
+            else
+            {
+                this.autor = autor;
+                this.titel = titel;
+                auflage = auflage;
+            }
+        }
 
+        public bool Result
+        {
+
+        }
+
+        public string Autor 
+        { 
+            get { return autor; } 
+            set { autor = value; } 
+        }
+        public string Titel
+        {
+            get { return titel; }
+            set { titel = value; }
+        }
+        public int Auflage
+        {
+            get { return auflage; }
+            set
+            {
+                if (this.auflage <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Auflage darf nicht kleiner als 1 sein!");
+                }
+                else
+                { 
+                     auflage = value;
+                }
+            }
+        }
     }
 }
