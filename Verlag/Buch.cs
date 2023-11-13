270938181b233eb03d5c54bdfcaf76edf355c8da
Vincent <vincent.k8@gso.schule.konl>
2023-11-13 12:06:57 +0100

    Konstruktor Buch nach Test #1 erstellt

---
+++ Verlag/Buch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verlag
+{
+    public class Buch
+    {
+        private string autor;
+        private string titel;
+        private int auflage;
+        public Buch (string autor, string titel, int auflage)
+        {
+            this.autor = autor;
+            this.titel = titel;
+            this.auflage = auflage;
+        }
+
+
+    }
+}
